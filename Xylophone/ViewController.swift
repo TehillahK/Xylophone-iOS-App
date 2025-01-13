e11455d27603f0bfce16c4875ca7b8a9de54a526
Tehillahk <kangambt@myumanitoba.ca>
2025-01-13 00:23:55 -0600

    play sound functionaility complete

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,64 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func playCKeyButton(_ sender: UIButton) {
+        playSound(key: "C")
+    }
+    
+    @IBAction func playDKeyButton(_ sender: UIButton) {
+        playSound(key: "D")
+    }
+    
+    @IBAction func playEKeyButton(_ sender: UIButton) {
+        playSound(key: "E")
+    }
+    
+    @IBAction func playFKeyButton(_ sender: UIButton) {
+        playSound(key: "F")
+    }
+    
+    
+    @IBAction func playGKeyButton(_ sender: UIButton) {
+        playSound(key: "G")
+    }
+    
+    
+    @IBAction func playAKeyButton(_ sender: UIButton) {
+        playSound(key: "A")
+    }
+    
+    @IBAction func playBKeyButton(_ sender: UIButton) {
+        playSound(key: "B")
+    }
+    
+    
+    
+    func playSound(key: String) {
+        
+       let url = Bundle.main.url(forResource: key, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+
+        player?.play()
+
+        
+    }
+    
+    
     
 
 }
+
+
+
 
