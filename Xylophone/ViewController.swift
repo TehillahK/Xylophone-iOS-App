bf99f220797ed06ee28cb3e11047fba32638fdfc
Tehillahk <kangambt@myumanitoba.ca>
2025-01-13 20:27:32 -0600

    using one action for everything

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -17,41 +17,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
-
-    @IBAction func playCKeyButton(_ sender: UIButton) {
-        playSound(key: "C")
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(key: sender.currentTitle!)
     }
-    
-    @IBAction func playDKeyButton(_ sender: UIButton) {
-        playSound(key: "D")
-    }
-    
-    @IBAction func playEKeyButton(_ sender: UIButton) {
-        playSound(key: "E")
-    }
-    
-    @IBAction func playFKeyButton(_ sender: UIButton) {
-        playSound(key: "F")
-    }
-    
-    
-    @IBAction func playGKeyButton(_ sender: UIButton) {
-        playSound(key: "G")
-    }
-    
-    
-    @IBAction func playAKeyButton(_ sender: UIButton) {
-        playSound(key: "A")
-    }
-    
-    @IBAction func playBKeyButton(_ sender: UIButton) {
-        playSound(key: "B")
-    }
-    
     
     
     func playSound(key: String) {
-        
        let url = Bundle.main.url(forResource: key, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
 
